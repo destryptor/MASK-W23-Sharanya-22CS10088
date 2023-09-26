e98ef46dc792e8b24cd3f6a8d6d850565c8281ac
destryptor <sharanya.chakraborty@kgpian.iitkgp.ac.in>
2023-09-26 18:04:49 +0530

    Server side validation and error handling added

--- Task ID B01-SRV/app.js
+++ Task ID B01-SRV/app.js
@@ -1,7 +1,7 @@
-//declaring constants and initialising them
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const { check, validationResult } = require("express-validator");
 
 const port = 80;
 
@@ -11,7 +11,6 @@ const options = {
     useUnifiedTopology: true,
 };
 
-//connecting to the MongoDB database
 mongoose.connect(uri, options)
     .then(() => {
         console.log("Connected to database 'TaskRound' successfully!");
@@ -20,27 +19,46 @@ mongoose.connect(uri, options)
         console.log("Error in connecting to database: ", error);
     });
 
-var db = mongoose.connection;
-
-//initialising the app
 const app = express();
 
-app.use(bodyParser.json());
 app.use(express.static('FrontEnd'));
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
-//defining the POST request
-app.post('/submit', function (req, res) {
-    var name = req.body.name;
-    var dob = req.body.dob;
-    var address = req.body.address;
-    var email = req.body.email;
-    var password = req.body.password;
-    var phone = req.body.phone;
+//validation rules
+var loginValidate = [
+    check('name').matches('^[A-Za-z ]+$').withMessage('Name should contain only alphabets.'),
 
-    var data = {
+    check('password')
+        .isLength({ min: 8 }).withMessage('Password should be at least 8 characters long.')
+        .matches('[0-9]').withMessage('Password Must Contain a Number')
+        .matches('[A-Z]').withMessage('Password Must Contain an Uppercase Letter')
+        .matches('[^A-Za-z0-9]').withMessage('Password must contain a special character'),
+
+    check('phone')
+        .matches('^[0-9]+$').withMessage('Phone number should have only digits (0-9)')
+        .isLength({ min: 10, max: 10 }).withMessage('Phone number should be 10 digits long')
+];
+
+//validation endpoint
+app.post('/validate', loginValidate, (req, res) => {
+    const errors = validationResult(req);
+
+    // console.log(errors.array());   //used this for debugging, which was just painful
+
+    if (!errors.isEmpty()) {
+        const errorMessages = errors.array().map(error => error.msg);
+        return res.status(422).json({ errors: errorMessages });
+    }
+
+    return res.status(200).send('Validation successful');
+});
+
+//form submission endpoint
+app.post('/submit', (req, res) => {
+    const { name, dob, address, email, password, phone } = req.body;
+
+    const data = {
         "name": name,
         "dob": dob,
         "address": address,
@@ -49,36 +67,35 @@ app.post('/submit', function (req, res) {
         "phone": phone
     }
 
-    //inserting the data into the database
+    const db = mongoose.connection;
+
     db.collection('FormDetails').insertOne(data, (error, result) => {
         if (error) {
             console.error('Error inserting data:', error);
+            return res.status(500).send('Error while inserting data into the database. Please try again later.');
         } else {
-            console.log('Data inserted successfully:', result);
+            console.log('Data inserted successfully: ', result);
+            return res.status(200).send('Data inserted successfully');
         }
     });
+});
 
-    //redirecting to a primitive 'thank you' page after form submission
-    return res.redirect('thank_you.html');
-})
-
-//a simple GET request to view the initial home page
-app.get('/', function (req, res) {
+//the few GET requests
+app.get('/', (req, res) => {
     res.status(200).redirect('home.html');
 });
 
-app.get('/about', function (req, res) {
+app.get('/about', (req, res) => {
     res.status(201).redirect('about.html');
 });
 
-app.get('/form', function (req, res) {
+app.get('/form', (req, res) => {
     res.set({
         'Access-control-Allow-Origin': '*'
     });
     return res.redirect('form.html');
-})
-
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+    console.log(`Server running on port ${port}`);
 });
